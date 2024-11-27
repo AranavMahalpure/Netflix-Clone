c75c1385b2ed1d4a2875e94e79ce82871a93cd53
fadkeabhi <31249309+fadkeabhi@users.noreply.github.com>
2024-11-27 22:54:20 +0530

    Remove footer links

--- src/components/Layout/Footer.jsx
+++ src/components/Layout/Footer.jsx
@@ -7,27 +7,27 @@ export function Footer() {
     <footer className="px-4 py-8 border-t border-gray-600">
       <div className="max-w-6xl mx-auto grid grid-cols-2 md:grid-cols-4 gap-8">
         <div className="space-y-4">
-          <a href="#" className="text-gray-400 hover:underline block">FAQ</a>
-          <a href="#" className="text-gray-400 hover:underline block">Investor Relations</a>
-          <a href="#" className="text-gray-400 hover:underline block">Privacy</a>
-          <a href="#" className="text-gray-400 hover:underline block">Speed Test</a>
+          <a className="text-gray-400 hover:underline block">FAQ</a>
+          <a className="text-gray-400 hover:underline block">Investor Relations</a>
+          <a className="text-gray-400 hover:underline block">Privacy</a>
+          <a className="text-gray-400 hover:underline block">Speed Test</a>
         </div>
         <div className="space-y-4">
-          <a href="#" className="text-gray-400 hover:underline block">Help Centre</a>
-          <a href="#" className="text-gray-400 hover:underline block">Jobs</a>
-          <a href="#" className="text-gray-400 hover:underline block">Cookie Preferences</a>
-          <a href="#" className="text-gray-400 hover:underline block">Legal Notices</a>
+          <a className="text-gray-400 hover:underline block">Help Centre</a>
+          <a className="text-gray-400 hover:underline block">Jobs</a>
+          <a className="text-gray-400 hover:underline block">Cookie Preferences</a>
+          <a className="text-gray-400 hover:underline block">Legal Notices</a>
         </div>
         <div className="space-y-4">
-          <a href="#" className="text-gray-400 hover:underline block">Account</a>
-          <a href="#" className="text-gray-400 hover:underline block">Ways to Watch</a>
-          <a href="#" className="text-gray-400 hover:underline block">Corporate Information</a>
-          <a href="#" className="text-gray-400 hover:underline block">Only on Netflix</a>
+          <a className="text-gray-400 hover:underline block">Account</a>
+          <a className="text-gray-400 hover:underline block">Ways to Watch</a>
+          <a className="text-gray-400 hover:underline block">Corporate Information</a>
+          <a className="text-gray-400 hover:underline block">Only on Netflix</a>
         </div>
         <div className="space-y-4">
-          <a href="#" className="text-gray-400 hover:underline block">Media Centre</a>
-          <a href="#" className="text-gray-400 hover:underline block">Terms of Use</a>
-          <a href="#" className="text-gray-400 hover:underline block">Contact Us</a>
+          <a className="text-gray-400 hover:underline block">Media Centre</a>
+          <a className="text-gray-400 hover:underline block">Terms of Use</a>
+          <a className="text-gray-400 hover:underline block">Contact Us</a>
         </div>
       </div>
       <div className="mt-8 text-center text-gray-400">
